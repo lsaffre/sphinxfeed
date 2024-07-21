8ff80dbe413dbc9a3212bad42dbc0c8bfb2f1eb8
Jordan Cook <jordan.cook.git@proton.me>
2024-07-21 12:20:10 -0500

    Use dateutil.parser instead of time.strptime

--- sphinxfeed.py
+++ sphinxfeed.py
@@ -7,9 +7,9 @@ See https://github.com/lsaffre/sphinxfeed
 __version__ = '0.3.4'
 
 import os.path
-import time
 from datetime import datetime
 from dateutil.tz import tzlocal
+from dateutil.parser import parse as parse_date
 from sphinx.util.logging import getLogger
 
 from feedgen.feed import FeedGenerator
@@ -17,23 +17,6 @@ from feedgen.feed import FeedEntry
 
 doc_trees = []  # for atelier
 logger = getLogger(__name__)
-
-
-def parse_pubdate(pubdate):
-    # tz = "+0002"
-    # chunks = pubdate.split()
-    # if len(chunks) == 1:
-    #     chunks.append("23:59")
-    # if len(chunks) == 2:
-    #     chunks.append(tz)
-    # fmt = '%Y-%m-%d %H:%M %Z'
-    # return time.strptime(' '.join(chunks), fmt)
-    fmt = '%Y-%m-%d %H:%M'
-    try:
-        date = time.strptime(pubdate, fmt)
-    except ValueError:
-        date = time.strptime(pubdate + " 23:59", fmt)
-    return date
 
 
 def setup(app):
@@ -84,21 +67,17 @@ def create_feed_item(app, pagename, templatename, ctx, doctree):
     env = app.builder.env
     metadata = app.builder.env.metadata.get(pagename, {})
 
-    pubDate = metadata.get(app.config.feed_field_name, None)
-    if not pubDate:
+    pubdate = metadata.get(app.config.feed_field_name, None)
+    if not pubdate:
         return
 
-    pubDate = parse_pubdate(pubDate)
-
-    if pubDate > time.localtime():
-        logger.info("Skipping %s, publish date is in the future: %s", pagename, pubDate)
+    pubdate = parse_date(pubdate).replace(tzinfo=tzlocal())
+    if pubdate > datetime.now(tzlocal()):
+        logger.info("Skipping %s, publish date is in the future: %s", pagename, pubdate)
         return
 
     if not ctx.get('body') or not ctx.get('title'):
         return
-
-    pubDate = datetime.fromtimestamp(time.mktime(pubDate))
-    pubDate = pubDate.replace(tzinfo=tzlocal())
 
     item = FeedEntry()
     item.title(ctx.get('title'))
@@ -109,7 +88,7 @@ def create_feed_item(app, pagename, templatename, ctx, doctree):
     if app.config.feed_use_atom:
         item.id(href)
     item.description(ctx.get('body'))
-    item.published(pubDate)
+    item.published(pubdate)
 
     if author := metadata.get('author'):
         # author may be a str (in field list/frontmatter) or a dict (expected by feedgen)
