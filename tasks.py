2b4dfb964521658e199f65ba986ff88b928309ae
Luc Saffre <luc.saffre@gmail.com>
2025-03-28 10:06:35 +0200

    fix inv test fails with NO TESTS RAN

--- tasks.py
+++ tasks.py
@@ -5,5 +5,6 @@ ns = setup_from_tasks(
     "sphinxfeed",
     blogref_url="https://luc.lino-framework.org",
     revision_control_system='git',
+    test_command="pytest tests",  # more options in file pytest.ini
     # tolerate_sphinx_warnings=True,
     cleanable_files=[])
